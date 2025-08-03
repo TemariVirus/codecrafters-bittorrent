23c5227f05b256ac54df8989b489be9de73880f1
TemariVirus <kiyonnxperiaz@gmail.com>
2025-08-03 16:15:20 +0800

    Decode bencoded integers #eb4

--- src/main.zig
+++ src/main.zig
@@ -12,32 +12,51 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    if (args.len < 3) {
-        try stdout.print("Usage: your_program.sh <command> <args>\n", .{});
-        std.process.exit(1);
-    }
+    if (args.len < 3) @panic("Usage: your_program.sh <command> <args>");
 
     const command = args[1];
 
     if (std.mem.eql(u8, command, "decode")) {
-        const encodedStr = args[2];
-        const decodedStr = decodeBencode(encodedStr) catch {
-            stdout.print("Invalid encoded value\n", .{}) catch {};
-            std.process.exit(1);
-        };
-        try stdout.print("{f}\n", .{std.json.fmt(decodedStr, .{})});
+        const encoded = args[2];
+        const decoded = Bencode.decode(encoded) catch @panic("Invalid encoded value");
+        switch (decoded) {
+            .string => |s| try stdout.print("{f}\n", .{std.json.fmt(s, .{})}),
+            .integer => |i| try stdout.print("{f}\n", .{std.json.fmt(i, .{})}),
+        }
     }
 }
 
-fn decodeBencode(encodedValue: []const u8) error{InvalidArgument}![]const u8 {
-    if (encodedValue[0] >= '0' and encodedValue[0] <= '9') {
-        const firstColon = std.mem.indexOf(u8, encodedValue, ":");
+const Bencode = union(enum) {
+    string: []const u8,
+    integer: i64,
+
+    pub const DecodeError = error{InvalidArgument};
+
+    pub fn decode(encoded: []const u8) DecodeError!Bencode {
+        if (encoded[0] >= '0' and encoded[0] <= '9') {
+            return decodeString(encoded);
+        } else if (encoded[0] == 'i') {
+            return decodeInteger(encoded);
+        } else @panic("Unsupported type");
+    }
+
+    fn decodeString(encoded: []const u8) DecodeError!Bencode {
+        const firstColon = std.mem.indexOf(u8, encoded, ":");
         if (firstColon == null) {
             return error.InvalidArgument;
         }
-        return encodedValue[firstColon.? + 1 ..];
-    } else {
-        stdout.print("Only strings are supported at the moment\n", .{}) catch {};
-        std.process.exit(1);
+
+        const len = std.fmt.parseInt(usize, encoded[0..firstColon.?], 10) catch return error.InvalidArgument;
+        return .{ .string = encoded[firstColon.? + 1 ..][0..len] };
     }
-}
+
+    fn decodeInteger(encoded: []const u8) DecodeError!Bencode {
+        const end = std.mem.indexOf(u8, encoded, "e");
+        if (end == null) {
+            return error.InvalidArgument;
+        }
+
+        const value = std.fmt.parseInt(i64, encoded[1..end.?], 10) catch return error.InvalidArgument;
+        return .{ .integer = value };
+    }
+};
